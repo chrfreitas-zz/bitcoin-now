75b456d401df8e4cf9065b0b9cd162f0a25a346f
Christian Freitas <chrfreitas@gmail.com>
2017-12-11 21:15:16 -0200

    Request new value of bitcoin

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-const http = require('http')
+const axios = require('axios')
+const console = require('console')
 
-const URL = 'https://blockchain.info/pt/ticker'
+const url = 'https://blockchain.info/pt/ticker',
+      currency = process.argv[2];
+
+axios.get(url).then(({ data }) => {
+    if(!currency) {
+        throw new Error('There is not parameters')
+    }
+
+    if(!data[currency]) {
+        throw new Error('This currency doens\'t exist')
+    }
+
+    console.log(data[currency].last);
+})
